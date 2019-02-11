5cc11411e1191c2768bcd216b9ddac4a6d7725ab
Marton Bur <marton.bur@gmail.com>
2019-02-10 21:04:46 -0500

    Adding facet_grid plot for query runtimes

--- sttt-cps-query/plot.r
+++ sttt-cps-query/plot.r
@@ -7,8 +7,33 @@ library(scales)
 load_results = function(filename) {
   print(filename)
   df <- read_delim(filename, delim="|")
-  df <- df[c("query_id", "model_size", "time_seconds")]
-  df <- rename(df, qi = query_id, ts = time_seconds)
+  df <- df[c("query_id", "model_size", "time_seconds","allocation_type")]
+  df <- select(df, qi = query_id, ms = model_size, ts = time_seconds, at = allocation_type)
   df
 }
 
+df <- bind_rows(
+  load_results("MoDeS3/CloseTrains.csv"),
+  load_results("MoDeS3/Derailment.csv"),
+)
+
+aggregated <- ddply(
+  .data = df,
+  .variables = c("ms", "qi"),
+)
+
+p <- ggplot(aggregated, aes_string(x="qi", y="ts")) #set variables here
+
+p <- p + geom_point(alpha = 1/5, size = 2.0, stroke = 0)
+
+p <- p + facet_grid(ms ~ at, drop=FALSE, scales="free") + #to match all diagram scales, set scales="fixed"
+  xlab("Query") +
+  ylab("Execution time [s]") +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5),
+    panel.spacing = unit(0.1, "lines"),
+    plot.margin=unit(c(1,1,1,1), "mm")
+  )
+
+ggsave(file="results.pdf", width=150, height=250, units="mm")
