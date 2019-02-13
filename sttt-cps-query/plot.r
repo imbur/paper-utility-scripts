edac325b7a4b629190a08430218ffd53f60bc646
Marton Bur <marton.bur@gmail.com>
2019-02-13 18:39:47 -0500

    Started empirical distribution plot

--- sttt-cps-query/plot.r
+++ sttt-cps-query/plot.r
@@ -4,7 +4,7 @@ library(scales)
 
 #install.packages("tidyverse")
 
-load_results = function(filename) {
+load_query_results = function(filename) {
   print(filename)
   df <- read_delim(filename, delim="|")
   df <- df[c("query_id", "model_size", "time_seconds","allocation_type")]
@@ -12,12 +12,21 @@ load_results = function(filename) {
   df
 }
 
-df <- data.frame()
 
-files <- list.files(path = "results/",pattern = ".csv")
+load_model_results = function(filename) {
+  print(filename)
+  df <- read_delim(filename, delim="|")
+  df <- df[c("model_size", "update_type", "timestamp_seconds", "delta_seconds", "object_count", "object_delta_count", "remote_reference_sent_ack_count", "remote_reference_sent_ack_delta_count", "remote_reference_recv_ack_count", "remote_reference_recv_ack_delta_count")]
+  df <- select(df, ms = model_size, ut = update_type, ts = timestamp_seconds, ds = delta_seconds, oc = object_count, odc = object_delta_count)
+  df
+}
+
+prefix <- "results/"
+files <- list.files(path = prefix,pattern = ".csv")
+df <- data.frame()
 for (filename in files) {
   df <- bind_rows(df, 
-    load_results(paste("results/",filename, sep = ""))
+    load_query_results(paste(prefix,filename, sep = ""))
   )
 }
 
@@ -41,3 +50,36 @@ p <- p + facet_grid(ms ~ at, drop=FALSE, scales="free") + #to match all diagram 
   )
 
 ggsave(file="results.pdf", width=150, height=250, units="mm")
+
+
+
+
+
+prefix <- "model-results/"
+files <- list.files(path = prefix ,pattern = ".csv")
+df <- data.frame()
+for (filename in files) {
+  df <- bind_rows(df, 
+    load_model_results(paste(prefix,filename, sep = ""))
+  )
+}
+
+objects = df$odc
+breaks = df$ts
+
+modelData <- select(df, "ms", "odc")
+
+#splitdata <- ddply(modelData, .variables = "ms", .fun = cumsum)
+splitdata <- ddply(df, .variables = "ms", summarise, cummulated = cumsum(odc), time = ts)
+
+splitdata[ms] <- sapply(splitdata$ms, as.character)
+
+ggplot(splitdata, aes(time, cummulated, colour=ms)) + geom_line(aes(group = ms))
+
+plot(splitdata$timestamp, splitdata$total_objects,            # plot the data 
+          main="Number of model objects",  # main title 
+          xlab="Duration minutes",        # x−axis label 
+          ylab="Cumulative eruptions")   # y−axis label 
+lines(breaks, cumobj)           # join the points 
+#lines(breaks, cumfreq0)           # join the points 
+
