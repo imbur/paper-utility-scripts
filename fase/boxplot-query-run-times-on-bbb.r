5ff9b7c711b6d0805a7b2a750761f557b7df940c
Marton Bur <marton.bur@gmail.com>
2020-12-11 00:59:36 -0500

    Add FASE local query execution results and plotter script

---
+++ fase/boxplot-query-run-times-on-bbb.r
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+wide <- read_csv("local-bbb-runtimes.csv")
+long <- gather(wide, run, time, -c(query, allocation, size, elements))
+
+long$query = factor(long$query, levels=c("Train Locations", "Close Trains", "Derailment", "End of Siding"))
+
+ggplot(long) +
+  geom_boxplot(aes_string(x = "query", y = "time")) +
+  facet_wrap(facets = long$elements, scale = "free_y", ncol = 4) +
+  xlab("Query") +
+  ylab("Execution time [s]") +
+  theme_bw() +
+  theme(
+    axis.text = element_text(size=7),
+    axis.text.x = element_text(angle=90)
+  )
+
+ggsave(file = "modes-execution-times-plot.pdf", width = 200, height = 70, units = "mm")
