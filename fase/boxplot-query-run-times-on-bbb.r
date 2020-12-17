dc9082841927d9caec3ae75c6a7bfe9247a4e963
Marton Bur <marton.bur@gmail.com>
2020-12-16 19:49:50 -0500

    Add plotting script for local query exec times on BBBs

--- fase/boxplot-query-run-times-on-bbb.r
+++ fase/boxplot-query-run-times-on-bbb.r
@@ -1,19 +1,29 @@
 library(tidyverse)
 
 wide <- read_csv("local-bbb-runtimes.csv")
-long <- gather(wide, run, time, -c(query, allocation, size, elements))
+long <-
+  gather(wide, run, time,-c(query, allocation, size, elements))
 
-long$query = factor(long$query, levels=c("Train Locations", "Close Trains", "Derailment", "End of Siding"))
+long$query = factor(long$query,
+                    levels = c("Train Locations", "Close Trains", "Derailment", "End of Siding"))
 
-ggplot(long) +
+exectimes = ggplot(long) +
   geom_boxplot(aes_string(x = "query", y = "time")) +
-  facet_wrap(facets = long$elements, scale = "free_y", ncol = 4) +
+  facet_wrap(facets = long$elements,
+             scale = "free_y",
+             ncol = 4) +
   xlab("Query") +
   ylab("Execution time [s]") +
   theme_bw() +
-  theme(
-    axis.text = element_text(size=7),
-    axis.text.x = element_text(angle=90)
-  )
+  theme(axis.text = element_text(size = 7),
+        axis.text.x = element_text(angle = 90))
 
-ggsave(file = "modes-execution-times-plot.pdf", width = 200, height = 70, units = "mm")
+exectimes
+
+ggsave(
+  plot = exectimes,
+  file = "modes-execution-times-plot.pdf",
+  width = 200,
+  height = 70,
+  units = "mm"
+)
--- tikzdevice-skeleton.r
+++ tikzdevice-skeleton.r
@@ -23,6 +23,12 @@ tikz('normal.tex', standAlone = FALSE, width = 2, height = 3)
 # tecs heatmap -- FIXME
 tikz('normal.tex', standAlone = FALSE, width = 2, height = 4)
 
+# thesis embedded query scalability plot
+tikz('normal.tex', standAlone = FALSE, width = 6, height = 2.5)
+
+# thesis BBB query scalability plot
+tikz('normal.tex', standAlone = FALSE, width = 5, height = 3)
+
 # do plotting then terminate session by dev.off()
 dev.off()
 
