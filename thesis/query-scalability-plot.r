106073eddca32bec1aafed45f38ec0565602c3d5
Marton Bur <marton.bur@gmail.com>
2020-12-16 19:52:26 -0500

    Finish embedded single query scalability plot

--- thesis/query-scalability-plot.r
+++ thesis/query-scalability-plot.r
@@ -3,6 +3,9 @@ library(plyr)
 library(scales)
 library(RColorBrewer)
 library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(data.table)
 
 load_query_results = function(filename, sf, uc) {
   print(filename)
@@ -36,6 +39,7 @@ load_query_results = function(filename, sf, uc) {
     )
   df$exectime <- as.numeric(df$exectime)
   df$uc <- toupper(df$uc)
+  df$uc <- factor(df$uc)
   df
 }
 
@@ -43,26 +47,54 @@ scale_factors = c(1, 2, 5, 10, 20)
 microcontroller_types = c("ATSAMD21G18A", "XMC4500", "STM32f767ZI")
 
 prefix <- "data/"
-files <- list.files(path = prefix , pattern = ".csv")
 df <- data.frame()
 for (sf in scale_factors) {
   for (uc in microcontroller_types) {
     df <-
       bind_rows(df, load_query_results(
-        paste(prefix, "modes3-", tolower(uc), "-query-eval-sf-", sf, ".csv", sep = ""),
+        paste(
+          prefix,
+          "modes3-",
+          tolower(uc),
+          "-query-eval-sf-",
+          sf,
+          ".csv",
+          sep = ""
+        ),
         sf,
         uc
       ))
   }
 }
 
-ggplot(data = df, aes(x = sf, y = exectime), as.table = T) +
+forced_levels = rev(levels(df$uc))
+tmp = forced_levels[3]
+forced_levels[3] = forced_levels[1]
+forced_levels[1] = tmp
+
+exectime_plot = ggplot(data = transform(df,
+                                        uc = factor(uc, levels = forced_levels)),
+                       aes(x = sf, y = exectime),
+                       as.table = T) +
   geom_line(aes(color = query)) +
   geom_point(aes(color = query)) +
   xlab("Model Size") +
   ylab("Execution Time (microseconds)") +
-  scale_x_continuous(trans='log10', breaks = c(1,2,5,10,20), labels = c(24, 48, 120, 240, 480)) +
-  scale_y_continuous(trans='log10') +
-  scale_fill_discrete(name = "Dose", labels = c("A", "B", "C", "D"))+
-  facet_wrap( ~ uc, ncol = 3, scales = "free_y") +
+  scale_x_continuous(
+    trans = 'log10',
+    breaks = c(1, 2, 5, 10, 20),
+    labels = c(24, 48, 120, 240, 480)
+  ) +
+  scale_y_continuous(trans = 'log10') +
+  facet_grid(. ~ uc) +
   scale_color_discrete(name = "Query")
+
+exectime_plot
+
+ggsave(
+  file = "~/git/phd/chapters/queries-at-runtime/figures/embedded-queries-scalability.pdf",
+  plot = exectime_plot,
+  width = 200,
+  height = 80,
+  units = "mm"
+)
