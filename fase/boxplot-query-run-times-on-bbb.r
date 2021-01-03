0b80b6a703fe22c478124c8c097a6f11ca76b275
Marton Bur <marton.bur@gmail.com>
2021-01-03 18:00:52 -0500

    Transition to ms from s

--- fase/boxplot-query-run-times-on-bbb.r
+++ fase/boxplot-query-run-times-on-bbb.r
@@ -7,13 +7,16 @@ long <-
 long$query = factor(long$query,
                     levels = c("Train Locations", "Close Trains", "Derailment", "End of Siding"))
 
+#convert to ms from the original s
+long$time = long$time*1000
+
 exectimes = ggplot(long) +
   geom_boxplot(aes_string(x = "query", y = "time")) +
   facet_wrap(facets = long$elements,
              scale = "free_y",
              ncol = 4) +
   xlab("Query") +
-  ylab("Execution time [s]") +
+  ylab("Execution time [ms]") +
   theme_bw() +
   theme(axis.text = element_text(size = 7),
         axis.text.x = element_text(angle = 90))
